d1da128acd4e244236bedb87f0d71fac535c39ee
Nikita <sorakield@gmail.com>
2026-01-10 17:43:25 +0300

    Swipe navigation styles fix

--- src/pages/SwipePage.tsx
+++ src/pages/SwipePage.tsx
@@ -113,13 +113,27 @@ function SwipePage() {
 			{/* Swipe Toolbar (Mobile Only) */}
 			<div className='swipePage__mobileToolbar'>
 				<Link to={`/events/${id}`} className='swipePage__toolbarBack'>
-					← Назад
+					<span>←</span> Назад
 				</Link>
 				<button
 					className='swipePage__toolbarFilterBtn'
 					onClick={() => setShowFilters(true)}
 				>
-					<span className='icon'>⚙️</span> Фильтры
+					<span className='icon'>
+						<svg
+							width='18'
+							height='18'
+							viewBox='0 0 24 24'
+							fill='none'
+							stroke='currentColor'
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'
+						>
+							<polygon points='22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3'></polygon>
+						</svg>
+					</span>{' '}
+					Фильтры
 				</button>
 			</div>
 
